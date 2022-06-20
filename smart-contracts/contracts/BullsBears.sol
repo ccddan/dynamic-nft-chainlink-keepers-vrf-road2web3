d9fd968078fd716fc2a27ce15144e9c180d81780
ccddan <ccdd.dev@gmail.com>
2022-06-19 23:31:55 -0500

    feat: add true randomness when price trend changes

--- smart-contracts/contracts/BullsBears.sol
+++ smart-contracts/contracts/BullsBears.sol
@@ -9,13 +9,16 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 contract BullsBears is
     ERC721,
     ERC721Enumerable,
     ERC721URIStorage,
     Ownable,
-    KeeperCompatibleInterface
+    KeeperCompatibleInterface,
+    VRFConsumerBaseV2
 {
     using Counters for Counters.Counter;
 
@@ -31,9 +34,19 @@ contract BullsBears is
 
     mapping(PriceTrend => string[]) private tokenUris;
 
+
+    // VRF Configuration
+    VRFCoordinatorV2Interface private immutable coordinator = VRFCoordinatorV2Interface(VRF_COORDINATOR);
+    uint64 private subscriptionId;
+    address private constant VRF_COORDINATOR = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    bytes32 private immutable keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 private immutable callbackGasLimit = 100000;
+    uint16 private immutable requestConfirmations = 3;
+    uint32 private numWords =  0; // changes dynamically very time setAllTokensUris is called
+
     event PriceTrendUpdated(string trend);
 
-    constructor(uint updateInterval, address btcPriceFeed) ERC721("Bulls & Bears", "BnB") {
+    constructor(uint updateInterval, address btcPriceFeed) ERC721("Bulls & Bears", "BnB") VRFConsumerBaseV2(VRF_COORDINATOR) {
         // keeper
         interval = updateInterval;
         lastTimestamp = block.timestamp;
@@ -59,7 +72,7 @@ contract BullsBears is
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
 
-        string memory defaultUri = getRandomTokenUri();
+        string memory defaultUri = getSemiRandomTokenUri();
         _setTokenURI(tokenId, defaultUri);
     }
 
@@ -94,8 +107,7 @@ contract BullsBears is
         return uint256(price);
     }
 
-    function getRandomTokenUri() private view returns(string memory) {
-        // TODO: add true randomness with Chainlink VRF
+    function getSemiRandomTokenUri() private view returns(string memory) {
         uint idx = uint(keccak256(
             abi.encodePacked(
                 block.number,
@@ -110,10 +122,15 @@ contract BullsBears is
 
     function setAllTokensUris() private {
         uint n = _tokenIdCounter.current();
-        for (uint idx = 0; idx < n; ++idx ) {
-            _setTokenURI(idx, getRandomTokenUri());
+        if (subscriptionId == 0) {
+            for (uint idx = 0; idx < n; ++idx ) {
+                _setTokenURI(idx, getSemiRandomTokenUri());
+            }
+            emit PriceTrendUpdated(priceTrend == PriceTrend.BULL ? "bull" : "bear");
+        } else {
+            numWords = uint32(n);
+            requestRandomNumbers();
         }
-        emit PriceTrendUpdated(priceTrend == PriceTrend.BULL ? "bull" : "bear");
     }
 
     // Chainlink Keepers
@@ -133,6 +150,30 @@ contract BullsBears is
                 setAllTokensUris();
                 currentPrice = lastPrice;
             }
+        }
+    }
+
+    // Chianlink VRF
+    function setVRFSubscriptionId(uint64 id) public onlyOwner {
+        subscriptionId = id;
+    }
+
+    function requestRandomNumbers() private onlyOwner {
+        coordinator.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] memory randomWords) internal override {
+        require(_tokenIdCounter.current() == randomWords.length, "Random numbers mismatch");
+
+        uint totalUniqueNfts = tokenUris[priceTrend].length;
+        for (uint idx = 0; idx < randomWords.length; ++idx) {
+            _setTokenURI(idx, tokenUris[priceTrend][randomWords[idx] % totalUniqueNfts]);
         }
     }
 }
